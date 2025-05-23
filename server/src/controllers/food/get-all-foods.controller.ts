2ca4b6ab5a7123cc018dd86390bf9883fca493ad
tugozz <tuguldurbayar05@gmail.com>
2025-05-23 09:51:29 +0800

    aa

--- server/src/controllers/food/get-all-foods.controller.ts
+++ server/src/controllers/food/get-all-foods.controller.ts
@@ -1,1 +1,12 @@
 import { Request, Response } from "express";
+import { foodOrderModel } from "../../models/food-order.model";
+
+export const getAllOrdersController = async (req: Request, res: Response) => {
+  const allOrders = await foodOrderModel
+    .find()
+    .limit(10)
+    .skip(10 * 2);
+
+  const total = await foodOrderModel.countDocuments();
+  res.status(200).send({ allOrders, total });
+};
